2bc0b886e785d087678ae2bee2ac2bc00d02ccd2
Miles-Blue <111085678+Miles-Blue@users.noreply.github.com>
2022-08-15 14:46:54 -0500

    Lots of Tests and Examples

---
+++ Chapter 2/Chapter 2 Programs.py
@@ -0,0 +1,44 @@
+def comment_example():
+    #comment_example receives no arguments
+    #it explains how to create a function header
+    #and outputs or returns "Hello world!".
+    print('Hello world!')
+
+def program2_1():
+    #program 2 1 receives no arguments
+    #it outputs three messages using apostrophes instead
+    #of quotation marks
+    print('Kate Austen')
+    print('123 Full Circle Drive')
+    print('Asheville, NC28899')
+    
+def program2_2():
+    #program 2 2 receives no arguments
+    #double quote output
+    print("Kate Austen")
+    print("123 Full Circle Drive")
+    print("Asheville, NC28899")
+    
+def program2_3():
+    #program 2 3 receives no arguments
+    #double quote output
+    #allows apostrophes within text
+    print("Don't fear!")
+    print("I'm here!")
+    
+def program2_4():
+    #program 2 4 receives no arguments
+    #apostrophe output
+    #allows quotations within text
+    print('Your assignment is to read "Hamlet" by tomorow.')
+    
+def program2_5():
+    #This function displays a person's name and address
+    print('Kate Austen')
+    print('123 Full Circle Drive')
+    print('Asheville, NC28899')
+    
+def program2_6():
+    print('Kate Austen') #Display the Name
+    print('123 Full Circle Drive') #Display the address
+    print('Asheville, NC28899') #Display the city, state, and ZIP
--- Chapter 2/Test Program.py
+++ Chapter 2/Test Program.py
@@ -3,3 +3,10 @@
 def print_something():
     #print a message
     print("This is a message outputted by print_something")
+    
+#another_function receives no arguments
+#it just outputs two messages separated by a blank line
+def another_function():
+    print("This is message 1")
+    print()
+    print("This is message 2")
