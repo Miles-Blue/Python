871a774d7f03a40881cf2a4315f9f6035c15a633
Miles-Blue <111085678+Miles-Blue@users.noreply.github.com>
2022-08-23 14:49:41 -0500

    Started Chapter three Programs

---
+++ Chapter 3/Chapter 3 Demo.py
@@ -0,0 +1,7 @@
+def demo_program():
+    bool(input("True or False "))
+    flag_var = True
+    flag_var = False
+
+    if flag_var:
+        print("True!")
---
+++ Chapter 3/Chapter 3 Programs.py
@@ -0,0 +1,46 @@
+def test_average():
+    #test average takes 3 inputs
+    #calculates average of 3 inputs
+    #if average is above 95, output a message
+    
+    HIGH_SCORE = 95
+    
+    #takes three inputs from user
+    score1 = float(input("Enter the first score: "))
+    score2 = float(input("Enter the second score: "))
+    score3 = float(input("Enter the third score: "))
+    
+    #calculates average of inputs
+    average = (score1 + score2 + score3) / 3
+    print("The average score is: ", format(average, '.2f'))
+    
+    #determines if average is above 95
+    #prints if true
+    if average > HIGH_SCORE:
+        print("Congragulations!")
+        print("That is a high score!")
+        
+def auto_repair_payroll():
+    #user inputs number of hours worked
+    #user inputs hourly pay rate
+    #if user worked more or less than 40 hours
+    #will determine gross pay with or without bonus
+    
+    BASE_HOURS = 40
+    
+    #user inputs number of hours worked and pay rate
+    hours_worked = float(input("Enter number of hours worked: "))
+    rate = float(input("Enter hourly rate: "))
+    
+    #determines if hours is above 40
+    #if it is, calculate for overtime
+    if hours_worked > BASE_HOURS:
+        extra_time = hours_worked - BASE_HOURS
+        gross_pay = (BASE_HOURS * rate) + 1.5 * (extra_time * rate)
+        print("You made: $" + format(gross_pay, '.2f'))
+    else:
+        gross_pay = hours_worked * rate
+        print("You made: $" + format(gross_pay, '.2f'))
+        
+        
+        
