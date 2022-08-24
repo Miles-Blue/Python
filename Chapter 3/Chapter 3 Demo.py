13f2c25d27aac972cd820251701fc2343dfab84e
Miles-Blue <111085678+Miles-Blue@users.noreply.github.com>
2022-08-24 14:49:30 -0500

    jeff

--- Chapter 3/Chapter 3 Demo.py
+++ Chapter 3/Chapter 3 Demo.py
@@ -5,3 +5,49 @@ def demo_program():
 
     if flag_var:
         print("True!")
+        
+def even_number():
+    number = int(input("Please enter an even number: "))
+    
+    if number % 2 == 0:
+        print("The number is even!")
+        #you could then perform the calculation
+    else:
+        print("I said to enter an even number!")
+        #do not perform the calculation
+    
+def range_of_numbers():
+    #range of numbers accepts no arguments
+    #it will prompt the user for a number from 1 to 10
+    #and output if the number is <> 5
+    #and outpu t if the number is out of range
+    
+    #get input from the user
+    number = int(input("Enter a number from 1 to 10: "))
+    
+    #check if the number is greater than 10
+    if number > 10:
+        print("Only enter numbers between 1 and 10.")
+    elif number <= 5:
+        print("Your number was between 1 and 5.")
+    elif number > 5:
+        print("Your number was between 6 and 10.")
+        
+def loan_qualify():
+    #loan qualify accepts no arguments
+    #program determines if one qualifies for a loan
+    #uses nested if statements
+    salary = float(input("Please enter your salary here: "))
+    
+    if salary >= 30000:
+        years_on_job = int(input("Please enter your years on job here: "))
+        if years_on_job >= 2:
+            print("You qualify for the loan.")
+        else:
+            print("You must have been on your current job for at least two years to qualify.")
+    else:
+        print("You must earn at least $30,000 per year to qualify.")
+        
+        
+        
+        
--- Chapter 3/Chapter 3 Programs.py
+++ Chapter 3/Chapter 3 Programs.py
@@ -44,3 +44,56 @@ def auto_repair_payroll():
         
         
         
+def password_verifier():
+    #password verifier takes no arguments
+    #user inputs a password
+    #if input doesn't match prospero, password is invalid
+    PASSWORD = "prospero"
+    
+    #gets input from user
+    password_input = input("Please enter the password: ")
+    
+    #determines if password matches, and what to do
+    if password_input == PASSWORD:
+        print("Password accepted.")
+    else:
+        print("Password is invalid.")
+        
+def sort_names():
+    #sort names takes no arguments
+    #user inputs two names
+    #program alphabetizes, and outputs them
+    
+    #takes user input, two names
+    name1 = str(input("Enter the first name (last, first): "))
+    name2 = str(input("Enter the second name (last, first): "))
+    
+    print("Here are the ames, sorted aphabetically.")
+    
+    #compare and outputthe names from greatest to least
+    if name1 < name2:
+        print(name1)
+        print(name2)
+    else:
+        print(name2)
+        print(name1)
+        
+def loan_qualify():
+    #loan qualify accepts no arguments
+    #program determines if one qualifies for a loan
+    #uses nested if statements
+    salary = float(input("Please enter your salary here: "))
+    
+    #determines if salary is enough
+    if salary >= 30000:
+        years_on_job = int(input("Please enter your years on job here: "))
+        if years_on_job >= 2: #determines if years on job is enough
+            print("You qualify for the loan.")
+        else:
+            print("You must have been on your current job for at least two years to qualify.")
+    else:
+        print("You must earn at least $30,000 per year to qualify.")
+        
+        
+        
+        
