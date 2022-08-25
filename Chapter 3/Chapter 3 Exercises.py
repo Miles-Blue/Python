3cc019d1649f23d620f657cba5e4bbd9933ce07e
Miles-Blue <111085678+Miles-Blue@users.noreply.github.com>
2022-08-25 14:48:57 -0500

    Started Chapter 3 exercises

---
+++ Chapter 3/Chapter 3 Exercises.py
@@ -0,0 +1,54 @@
+def day_converter():
+    #day converter accepts no arguments
+    #prompts user to input a number between 1 and 7
+    #program converts input into a day of the week in spanish
+    
+    #user inputs number between 1 and 7
+    day = int(input("Enter day of the week(Monday-Sunday) from 1-7: "))
+    
+    #determines the day and outputs it in spanish
+    if day == 1:
+        print("Today is 'Lunes'")
+    elif day == 2:
+        print("Today is 'Martes'")
+    elif day == 3:
+        print("Today is 'Miércoles'")
+    elif day == 4:
+        print("Today is 'Jueves'")
+    elif day == 5:
+        print("Today is 'Viernes'")
+    elif day == 6:
+        print("Today is 'Sábado'")
+    elif day == 7:
+        print("Today is 'Domingo'")
+    else:
+        print(day,"Is not a valid number.")
+        
+def roman_numerals():
+    #roman numerals accepts no arguments
+    #prompts the user to input a number 1-10
+    #program outputs the respective roman number
+    
+    #user inputs number 1-10
+    r_number = int(input("Enter a number 1-10: "))
+    
+    #determines the day and outputs it in spanish
+    if day == 1:
+        print("")
+    elif day == 2:
+        print("")
+    elif day == 3:
+        print("")
+    elif day == 4:
+        print("")
+    elif day == 5:
+        print("")
+    elif day == 6:
+        print("")
+    elif day == 7:
+        print("")
+    else:
+        print(r_number,"Is not a valid number.")
+        
+        
+        
--- Chapter 3/Chapter 3 Programs.py
+++ Chapter 3/Chapter 3 Programs.py
@@ -86,6 +86,7 @@ def loan_qualify():
     
     #determines if salary is enough
     if salary >= 30000:
+        print("Your salary meets the requirements.")
         years_on_job = int(input("Please enter your years on job here: "))
         if years_on_job >= 2: #determines if years on job is enough
             print("You qualify for the loan.")
@@ -94,6 +95,134 @@ def loan_qualify():
     else:
         print("You must earn at least $30,000 per year to qualify.")
         
+def grader():
+    #grader accepts no arguments
+    #user inputs a test score
+    #program determines the letter grade
+    grade = int(input("Enter your test grade here: "))
+    
+    #goes through if statements to determine letter grade
+    if grade >= 90:
+        print("Your grade is A.")
+    else:
+        if grade >= 80:
+            print("Your grade is B.")
+        else:        
+            if grade >= 70:
+                print("Your grade is C.")
+            else:
+                if grade >= 60:
+                    print("Your grade is D.")
+                else:
+                    print("Your grade is F.")
+                    
+def grader2():
+    #grader accepts no arguments
+    #user inputs a test score
+    #program determines the letter grade
+    grade = int(input("Enter your test score here: "))
+    
+    #goes through if statements to determine letter grade
+    if grade >= 90:
+        print("Your grade is A.")
+    elif grade >= 80:
+        print("Your grade is B.")
+    elif grade >= 70:
+        print("Your grade is C.")
+    elif grade >= 60:
+        print("Your grade is D.")
+    else:
+        print("Your grade is F.")
         
+def loan_qualify2():
+    #loan qualify accepts no arguments
+    #program determines if one qualifies for a loan
+    #uses nested if statements
+    salary = float(input("Please enter your salary here: "))
+    years_on_job = int(input("Please enter your years on job here: "))
+    
+    #determines if salary is enough
+    if salary >= 30000 and years_on_job >= 2:
+        print("You qualify for the loan.")
+    else:
+        print("You must earn at least $30,000 per year to qualify.")
+        print("You have to work for at least 2 years to qualify.")
         
+def loan_qualify3():
+    #loan qualify accepts no arguments
+    #program determines if one qualifies for a loan
+    #uses nested if statements
+    salary = float(input("Please enter your salary here: "))
+    years_on_job = int(input("Please enter your years on job here: "))
+    
+    #determines if salary is enough
+    if salary >= 30000 or years_on_job >= 2:
+        print("You qualify for the loan.")
+    else:
+        print("You must earn at least $30,000 per year to qualify.")
+        print("You have to work for at least 2 years to qualify.")
         
+def hit_the_target():
+    #hit the target accepts no arguments
+    #user inputs a direction to aim turtle
+    #turtle goes forward and if it hits target, congrats
+    
+    
+    #Named constants
+    SCREEN_WIDTH = 600
+    SCREEN_HEIGHT = 600
+    TARGET_LLEFT_X = 100
+    TARGET_LLEFT_Y = 250
+    TARGET_WIDTH = 25
+    FORCE_FACTOR = 30
+    PROJECTILE_SPEED = 1
+    NORTH = 90
+    SOUTH = 270
+    EAST = 0
+    WEST = 180
+    
+    #set up turtle
+    import turtle as tur
+    tur.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
+    
+    #Draw the target
+    tur.hideturtle()
+    tur.speed(0)
+    tur.penup()
+    tur.goto(TARGET_LLEFT_X, TARGET_LLEFT_Y)
+    tur.pendown()
+    tur.setheading(EAST)
+    tur.forward(TARGET_WIDTH)
+    tur.setheading(NORTH)
+    tur.forward(TARGET_WIDTH)
+    tur.setheading(WEST)
+    tur.forward(TARGET_WIDTH)
+    tur.setheading(SOUTH)
+    tur.forward(TARGET_WIDTH)
+    tur.penup()
+    
+    #resets turtle position
+    tur.goto(0, 0)
+    tur.setheading(EAST)
+    tur.pendown()
+
+    #user inputs angle and force
+    angle = float(input("Enter the desired angle: "))
+    force = float(input("Enter the desired force: "))
+
+    #calculates the distance
+    distance = force * FORCE_FACTOR
+    
+    #moves the turtle
+    tur.left(angle)
+    tur.forward(distance)
+    
+    #determines where the turtle lands and
+    #determines if it landed in the target
+    if tur.xcor() >= 100 and tur.xcor() <= 125\
+    and tur.ycor() >= 250 and tur.ycor() <= 275:
+        print("You hit the target!")
+    else:
+        print("You missed the target!")
+    tur.done()
+
