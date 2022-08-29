52714cb766cfaee043e98f4cd137789ea70e2554
Miles-Blue <111085678+Miles-Blue@users.noreply.github.com>
2022-08-29 14:43:08 -0500

    L

--- Chapter 3/Chapter 3 Exercises.py
+++ Chapter 3/Chapter 3 Exercises.py
@@ -95,28 +95,118 @@ def hot_dog():
     
     #determines how many hotdogs are needed
     total = people * hdog
+    pkg_buns = total / BUNS_PKG
+    pkg_hdog = total / HDOG_PKG
     buns_left = total % 8
     hdog_left = total % 10
+    buns_extra = 8 * int(pkg_buns + 1) - total
+    hdog_extra = 10 * int(pkg_hdog + 1) - total
     
+    #determines and outputs hotdogs, buns, hotdogs left, and buns left
     if buns_left == 0 and hdog_left == 0:
-        print("You will need", int(total / BUNS_PKG), "packages of buns.")
-        print("You will need", int(total / HDOG_PKG), "packages of hotdogs.")
-    elif buns_left != 0:
-        print("You will need", (int(total / BUNS_PKG) + 1),"packages of buns with", buns_left, "buns left.")
-        print("You will need", int(total / HDOG_PKG), "packages of hotdogs.")
-    elif hdog_left != 0:
-        print("You will need", int(total / BUNS_PKG), "packages of buns.")
-        print("You will need", (int(total / HDOG_PKG) + 1),"packages of buns with", hdog_left, "hotdogs left.")
+        print("You will need", int(pkg_buns), "packages of buns.")
+        print("You will need", int(pkg_hdog), "packages of hotdogs.")
+    elif buns_left != 0 and hdog_left == 0:
+        print("You will need", int(pkg_buns + 1),"packages of buns with", int(buns_extra), "bun(s) left.")
+        print("You will need", int(pkg_hdog), "packages of hotdogs.")
+    elif hdog_left != 0 and buns_left == 0:
+        print("You will need", int(pkg_buns), "packages of buns.")
+        print("You will need", int(pkg_hdog + 1),"packages of buns with", int(hdog_extra), "hotdog(s) left.")
     elif buns_left != 0 and hdog_left != 0:
-        print("You will need", (int(total / BUNS_PKG) + 1),"packages of buns with", buns_left, "buns left.")
-        print("You will need", (int(total / HDOG_PKG) + 1),"packages of buns with", hdog_left, "hotdogs left.")
+        print("You will need", int(pkg_buns + 1),"packages of buns with", int(buns_extra), "bun(s) left.")
+        print("You will need", int(pkg_hdog + 1),"packages of buns with", int(hdog_extra), "hotdog(s) left.")
 
+def time_calculator():
+    #time calculator accepts no arguments
+    #prompts user to input a random number
+    #program then calculates those seconds into
+    #days, hours, minutes, seconds
     
+    DAY = 86400
+    HOUR = 3600
+    MINUTE = 60
     
+    #prompts user to input seconds
+    seconds = int(input("Enter a number of seconds. "))
     
+    #determines days, hours, minutes, and seconds
+    #then outputs accordingly
+    if seconds >= 0:
+        if seconds >= DAY:
+            print(int(seconds / DAY), "day(s).")
+            day_remainder = seconds % DAY
+            if day_remainder >= HOUR:
+                print(int(day_remainder / HOUR), "hour(s).")
+                hour_remainder = day_remainder % HOUR
+                if hour_remainder >= MINUTE:
+                    print(int(hour_remainder / MINUTE), "minute(s).")
+                    minute_remainder = hour_remainder % MINUTE
+                    if minute_remainder > 0:
+                        print(minute_remainder, "second(s).")
+                    else:
+                        print()
+                else:
+                    print(hour_remainder, "second(s).")
+            else:
+                if day_remainder >= MINUTE:
+                    print(int(day_remainder / MINUTE), "minute(s).")
+                    minute_remainder = day_remainder % MINUTE
+                    if minute_remainder > 0:
+                        print(minute_remainder, "second(s).")
+                    else:
+                        print()
+                else:
+                    print(day_remainder, "second(s).")
+        else:
+            if seconds >= HOUR:
+                print(int(seconds / HOUR), "hour(s).")
+                hour_remainder = seconds % HOUR
+                if hour_remainder >= MINUTE:
+                    print(int(hour_remainder / MINUTE), "minute(s).")
+                    minute_remainder = hour_remainder % MINUTE
+                    if minute_remainder > 0:
+                        print(minute_remainder, "second(s).")
+                    else:
+                        print()
+                else:
+                    print(hour_remainder, "second(s).")
+            else:
+                if seconds >= MINUTE:
+                    print(int(seconds / MINUTE), "minute(s).")
+                    minute_remainder = seconds % MINUTE
+                    if minute_remainder > 0:
+                        print(minute_remainder, "second(s).")
+                    else:
+                        print()
+                else:
+                    print(seconds, "second(s).")
+    else:
+        print("That's not a valid answer silly billy.")
     
+def leap_year():
+    #leap year accepts no arguments
+    #prompts user to enter a year
+    #program then determines if it is a leap year
+    #then outputs the answer plus number of days in February
     
+    years = int(input("Enter a year here: "))
     
+    #determines if number inputted is a leap year or not
+    if years >= 0:
+        if years % 100 == 0:
+            if years % 400 == 0:
+                print(years, "is a leap year. There are 29 days in the month of February.")
+            else: 
+                print(years, "is not a leap year. There are 28 days in the month of February.")
+        else:
+            if years % 4 == 0:
+                print(years, "is a leap year. There are 29 days in the month of February.")
+            else:
+                print(years, "is not a leap year. There are 28 days in the month of February.")
+    else:
+        print(years, "is not a valid number.")
+        
+
     
     
     
